77b2f5c28285903d047357944574f2b56ca8f942
Pedro <peferrarida@gmail.com>
2025-09-27 18:41:47 -0300

    terceiro commit

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     titulo = 'Página inicial'
-    return render_template('index.html',titulo=titulo)
+    return render_template('index.html', titulo=titulo)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        if email == 'admin@example.com' and senha == '1234':
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', errou=True)
+
+    return render_template('login.html', errou=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
